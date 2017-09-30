bed5f956b21729e8782b3ae1204f4cab41e1f257
ReggieYang <1937730499@qq.com>
2017-09-30 10:47:47 -0400

    Data for user

--- static/map.js
+++ static/map.js
@@ -1,39 +1,47 @@
 //init map
 console.log(document.getElementById('map'))
 var citymap = {
-    dallas: {
-      center: {lat: 32.7833333, lng: -96.8},
-      population: 137
-    },
-    sanantonio: {
-      center: {lat: 29.4238889, lng: -98.4933333},
-      population: 245
-    },
-    miami: {
-      center: {lat: 25.7738889, lng: -80.1938889},
-      population: 82
-    },
-    milwaukee: {
-      center: {lat: 43.0388889, lng: -87.9063889},
-      population: 127
-    },
-    saltlakecity: {
-      center: {lat:  40.7608333, lng: -111.8902778},
-      population: 231
-    },
-    oklahomacity: {
-      center: {lat: 35.4675, lng: -97.5161111},
-      population: 23
-    },        
-    jackson: {
-      center: {lat: 32.2986111, lng: -90.1847222},
-      population: 65
-    },        
-    columbus: {
-      center: {lat: 39.9611111, lng:  -82.9988889},
-      population: 199
-    }
-  };
+  dallas: {
+    center: {lat: 32.7833333, lng: -96.8},
+    population: 137,
+    relation: 50
+  },
+  sanantonio: {
+    center: {lat: 29.4238889, lng: -98.4933333},
+    population: 245,
+    relation: 120
+  },
+  miami: {
+    center: {lat: 25.7738889, lng: -80.1938889},
+    population: 82,
+    relation: 170
+  },
+  milwaukee: {
+    center: {lat: 43.0388889, lng: -87.9063889},
+    population: 127,
+    relation: 50
+  },
+  saltlakecity: {
+    center: {lat:  40.7608333, lng: -111.8902778},
+    population: 231,
+    relation: 120
+  },
+  oklahomacity: {
+    center: {lat: 35.4675, lng: -97.5161111},
+    population: 23,
+    relation: 170
+  },
+  jackson: {
+    center: {lat: 32.2986111, lng: -90.1847222},
+    population: 65,
+    relation: 50
+  },
+  columbus: {
+    center: {lat: 39.9611111, lng:  -82.9988889},
+    population: 199,
+    relation: 120
+  }
+};
 
   function initMap() {
     // Create the map.
@@ -52,24 +60,22 @@ var citymap = {
         strokeOpacity: 0.8,
         strokeWeight: 2,
         fillColor: '#FFFF00',
-        fillOpacity: 0.35,
+        fillOpacity: 0.5,
         map: map,
         center: citymap[city].center,
         radius: Math.sqrt(citymap[city].population) * 14000
       });
-      // alert(citymap[city].population)
       if (citymap[city].population < 100) {
-        cityCircle.strokeColor = '#FFCC00'
-        cityCircle.fillColor = '#FFCC00'
+        cityCircle.strokeColor = '#669900'
+        cityCircle.fillColor = '#669900'
       }
-      else if (citymap[city].population > 150) {
-        cityCircle.strokeColor = '#FF6600'
-        cityCircle.fillColor = '#FF6600'
+      else if (citymap[city].population >= 100 && citymap[city].population < 150) {
+        cityCircle.strokeColor = '#66CC00'
+        cityCircle.fillColor = '#66CC00'
       }
       else {
-        cityCircle.strokeColor = '#FF0000'
-        cityCircle.fillColor = '#FF0000'
+        cityCircle.strokeColor = '#00FF00'
+        cityCircle.fillColor = '#00FF00'
       }
-      // alert(cityCircle.strokeColor)
     }
   }
