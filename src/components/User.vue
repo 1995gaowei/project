7ef1bd79f0678c492dc8cd39933581fec29a6746
woshikemizhende <15850535810@163.com>
2017-09-30 22:56:56 +0800

    modify user page

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="user">
-        <Row  type="flex" align="top" class="item">
+        <!-- <Row  type="flex" align="top" class="item">
             <Col :span="6" offset="3">
                 <div class="title">Name</div>
             </Col>
@@ -42,7 +42,41 @@
                     <Tag type="dot" color="blue"  v-for="item in user.otherPref" :key="item">{{ item }}</Tag>
                 </div>
             </Col>
-        </Row>
+        </Row> -->
+
+        <h1 style="text-align: center; margin-bottom: 30px;">{{ user.name }}</h1>
+
+        <div class="head">
+            <div class="line"></div>
+            <span>Address</span>
+        </div>
+        <div class="content">
+            {{ user.address }}
+        </div>
+
+        <div class="head">
+            <div class="line"></div>
+            <span>Skills</span>
+        </div>
+        <div class="content">
+            <div>
+                    <Tag type="dot" color="green"  v-for="item in user.skill" :key="item">{{ item }}</Tag>
+                    <Button type="dashed" icon="plus-round"></Button>
+                </div>
+        </div>
+
+        <div class="head">
+            <div class="line"></div>
+            <span>Preferences</span>
+        </div>
+        <div class="content">
+            <div>
+                    <Tag type="dot" color="blue"  v-for="item in user.preferences" :key="item">{{ item }}</Tag>
+                </div>
+                <div style="margin-top: 20px;">
+                    <Tag type="dot" color="blue"  v-for="item in user.otherPref" :key="item">{{ item }}</Tag>
+                </div>
+        </div>
         
     </div>
 </template>
@@ -82,5 +116,28 @@ export default {
 .value {
     font-size: 18px;
     font-weight: bold;
+}
+
+.head {
+    text-align: center;
+    position: relative;
+}
+.head span {
+    font-size: 18px;
+    padding: 0 10px;
+    background-color: white;
+    position: relative;
+}
+.head .line {
+    border-top: 1px solid #dddee1;
+    position: absolute;
+    top: 13px;
+    width: 100%;
+}
+.content {
+    font-size: 20px;
+    font-weight: bold;
+    text-align: center;
+    margin: 20px 0 50px 0;
 }
 </style>
